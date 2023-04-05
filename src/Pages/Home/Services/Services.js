906b8b3b852973286c3dc404bae7d64204f95822
abdullahtansen <abdullahaltansen899@gmail.com>
2023-04-05 21:04:16 +0600

    search added system

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,14 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
   const [isAsc,setIsAsc] = useState(true);
+  const [search,setSearch] = useState('');
+  const searchRef = useRef();
   useEffect(() => {
-    fetch(`http://localhost:5000/services?order=${isAsc ? 'asc' : 'desc'}`)
+    fetch(`http://localhost:5000/services?search=${search}&order=${isAsc ? 'asc' : 'desc'}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, [isAsc]);
+  }, [isAsc,search]);
+
+  const handleSearch =()=>{
+    setSearch(searchRef.current.value)
+  }
+
   return (
     <div>
       <div className="text-center mb-4">
@@ -19,6 +26,7 @@ const Services = () => {
           <br />
           or randomised words which don't look even slightly believable.
         </p>
+        <input placeholder="enter your product name" ref={searchRef} className="input" type="text" /> <button onClick={handleSearch} className="btn btn-ghost">search</button>
       {/* Sorted */}
         <div className="dropdown">
           <label tabIndex={0} className="btn m-1">Sorted</label>
